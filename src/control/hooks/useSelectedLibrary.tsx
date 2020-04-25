e9a4b318e76dd2ac9830c71b41ff8bd611eef996
Sergnio <sergnio@gmail.com>
2020-04-25 11:12:56 -0500

    Added shared components

---
+++ src/control/hooks/useSelectedLibrary.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {Library} from "../../model/library";
+
+function useSelectedLibrary(library: Library): Library {
+    const [selectedLibrary, setSelectedLibrary] = useState(Library.None)
+
+    return selectedLibrary;
+}
+
+export default useSelectedLibrary;
