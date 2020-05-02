7af47e8278049368f88937e9d9ca04233a88147c
Aaron.Berndt <Aaron.Berndt@target.com>
2020-05-02 11:49:43 -0500

    Added globalStyles and changed libview to use material ui

--- src/library-list/library-list.tsx
+++ src/library-list/library-list.tsx
@@ -1,39 +1,49 @@
-import {Library} from "../model/library";
+import { Library } from "../model/library";
 import LibraryOverview from "./components/library-overview";
 import React from "react";
 import AddLibrary from "./components/add-library";
-
+import { Divider, Card, CardHeader, CardContent } from "@material-ui/core";
+import useStyles from "../global-styles";
 interface LibraryListProps {
-    libraries: Library[]
-    onLibraryClick(library: Library): void
-    onAddLibraryClick(): void,
-    showAddLibraryComponent: boolean,
-    onAddLibraryCancel(): void,
-    onAddLibrarySubmit(library: Library): void,
+  libraries: Library[];
+  onLibraryClick(library: Library): void;
+  onAddLibraryClick(): void;
+  showAddLibraryComponent: boolean;
+  onAddLibraryCancel(): void;
+  onAddLibrarySubmit(library: Library): void;
 }
 
 function LibraryList(props: LibraryListProps) {
-    const { showAddLibraryComponent, onLibraryClick, onAddLibraryCancel, onAddLibrarySubmit, onAddLibraryClick, libraries } = props;
+  const {
+    showAddLibraryComponent,
+    onLibraryClick,
+    onAddLibraryCancel,
+    onAddLibrarySubmit,
+    onAddLibraryClick,
+    libraries,
+  } = props;
 
-    return (
-        <div>
-            <h1>Libraries</h1>
-            <div>
-                {libraries.map((lib: Library) => <LibraryOverview library={lib}
-                                                                        key={lib.id}
-                                                                        onClick={onLibraryClick}/>)}
-            </div>
-            <div>
-                <button onClick={onAddLibraryClick}>+</button>
-            </div>
-            {
-                showAddLibraryComponent &&
-                <AddLibrary onCancel={onAddLibraryCancel}
-                onSubmit={onAddLibrarySubmit}/>
-            }
-        </div>
+  const { cardHeader } = useStyles();
 
-    )
+  return (
+    <div>
+      <Card variant="outlined">
+        <CardHeader title="Libraries" className={cardHeader} />
+        <CardContent>
+          {libraries.map((lib: Library) => (
+            <>
+              <LibraryOverview
+                library={lib}
+                key={lib.id}
+                onClick={onLibraryClick}
+              />
+              <Divider />
+            </>
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default LibraryList;
