53cf633bb2bd1f555f485b1650ea8003762d77dd
Sergnio <sergnio@gmail.com>
2020-05-06 21:03:12 -0500

    Centered list

--- src/library-list/library-list.tsx
+++ src/library-list/library-list.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import AddLibrary from "./components/add-library";
 import { Divider, Card, CardHeader, CardContent } from "@material-ui/core";
 import useStyles from "../global-styles";
+import Grid from "@material-ui/core/Grid/Grid";
 interface LibraryListProps {
   libraries: Library[];
   onLibraryClick(library: Library): void;
@@ -15,33 +16,35 @@ interface LibraryListProps {
 
 function LibraryList(props: LibraryListProps) {
   const {
-    showAddLibraryComponent,
     onLibraryClick,
-    onAddLibraryCancel,
-    onAddLibrarySubmit,
-    onAddLibraryClick,
     libraries,
   } = props;
 
   const { cardHeader } = useStyles();
 
+
+
   return (
     <div>
-      <Card variant="outlined">
-        <CardHeader title="Libraries" className={cardHeader} />
-        <CardContent>
-          {libraries.map((lib: Library) => (
-            <>
-              <LibraryOverview
-                library={lib}
-                key={lib.id}
-                onClick={onLibraryClick}
-              />
-              <Divider />
-            </>
-          ))}
-        </CardContent>
-      </Card>
+      <Grid container justify="center">
+          <Grid item xs={10}>
+              <Card variant="outlined">
+                  <CardHeader title="Libraries" className={cardHeader} />
+                  <CardContent>
+                      {libraries.map((lib: Library) => (
+                          <>
+                              <LibraryOverview
+                                  library={lib}
+                                  key={lib.id}
+                                  onClick={onLibraryClick}
+                              />
+                              <Divider />
+                          </>
+                      ))}
+                  </CardContent>
+              </Card>
+          </Grid>
+      </Grid>
     </div>
   );
 }
