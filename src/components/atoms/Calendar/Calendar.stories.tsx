5f6ed1f97626502d851e9727bf2ee679e1569ed7
Sergio Najera <sergio.najera@target.com>
2020-07-16 20:29:14 -0500

    IFrame integration POC

---
+++ src/components/atoms/Calendar/Calendar.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Calendar from "./Calendar"
+
+export default {
+    title: "Test/Calendar",
+    component: Calendar,
+};
+export const withProps = () => (
+    <Calendar/>
+);
