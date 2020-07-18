27c2f213dcc1458b2f3040030fef345ca8f24d82
Sergnio <sergnio@gmail.com>
2020-07-18 10:09:04 -0500

    Added time grid - able toggle between month / week / day

--- src/components/atoms/Calendar/FullCalendar.tsx
+++ src/components/atoms/Calendar/FullCalendar.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import {events} from "./calendarConstants";
 
 export default () => (
     <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
+        headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
         events={events}
     />
 )
