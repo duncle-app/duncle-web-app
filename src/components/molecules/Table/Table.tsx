b05750a8e1b29e4f23a198d120abb62962431904
Sergnio <sergnio@gmail.com>
2020-08-29 16:48:32 -0500

    Changed colors to be easier on the eyes

--- src/components/molecules/Table/Table.tsx
+++ src/components/molecules/Table/Table.tsx
@@ -54,14 +54,16 @@ export default ({libraries, onEdit}: TableProps) => {
 
         // Need this to perform comparisons
         const momentDate = moment(meetingTime)
-        const green = "#4caf50";
+        const green = "rgba(76,175,80,0.68)";
+        const red = "rgba(219,16,16,0.78)";
+        const yellow = "rgba(255,244,57,0.64)";
 
         if (momentDate.isBefore(currentDate, 'month')) {
-            return "red"
+            return red
         } else if (momentDate.isSame(currentDate, 'month')) {
             return green
         } else if (momentDate.isSame(nextMonth, 'month')) {
-            return "yellow";
+            return yellow;
         } else {
             return "white"
         }
