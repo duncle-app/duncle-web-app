29cad0bd74637d5780d0c0fe1f85a46296928650
Sergnio <sergnio@gmail.com>
2020-09-04 09:13:51 -0500

    Fixed on close

--- src/components/atoms/Snackbar/Snackbar.tsx
+++ src/components/atoms/Snackbar/Snackbar.tsx
@@ -1,27 +1,27 @@
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert, {AlertProps} from '@material-ui/lab/Alert';
 import React, {useContext} from "react";
-import {GlobalContext, initialState as initialMessageState} from "../../../common/GlobalContext";
+import {isEqual} from 'lodash'
+import {GlobalContext, initialMState as initialMessageState} from "../../../common/GlobalContext";
 
 function Alert(props: AlertProps) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 export default function () {
-    const {message, setMessage, doggie} = useContext(GlobalContext)
-
-    console.log({doggie})
+    const {doggie, setdoggie} = useContext(GlobalContext)
+    const isOpen: boolean = !isEqual(doggie.message, initialMessageState.message)
 
     const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
 
-        setMessage(initialMessageState);
+        setdoggie(initialMessageState);
     };
 
     return (
-        <Snackbar open={doggie.message !== initialMessageState} autoHideDuration={4000} onClose={handleClose}>
+        <Snackbar open={isOpen} autoHideDuration={4000} onClose={handleClose}>
             <Alert onClose={handleClose} severity={doggie.severity}>
                 {doggie.message}
             </Alert>
