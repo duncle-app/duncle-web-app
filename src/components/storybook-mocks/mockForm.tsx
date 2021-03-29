0d74bf77d115d7aeae72eda7c98884a69154a842
sergnio <sergnioa@gmail.com>
2021-03-29 17:43:04 -0500

    changed DP to date time picker

--- src/components/storybook-mocks/mockForm.tsx
+++ src/components/storybook-mocks/mockForm.tsx
@@ -1,19 +1,14 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { Form } from "react-final-form";
 import { action } from "@storybook/addon-actions";
 
-// todo - make all forms like this, so I don't have to copy pasta everything
-function MockForm(props: any) {
-  return (
-    <Form
-      onSubmit={action("submit")}
-      render={({ handleSubmit }) => (
-        <>
-          <form onSubmit={handleSubmit}>{props.children}</form>
-        </>
-      )}
-    />
-  );
-}
-
-export default MockForm;
+export default ({ children }: PropsWithChildren<any>) => (
+  <Form
+    onSubmit={action("submit")}
+    render={({ handleSubmit }) => (
+      <>
+        <form onSubmit={handleSubmit}>{children}</form>
+      </>
+    )}
+  />
+);
